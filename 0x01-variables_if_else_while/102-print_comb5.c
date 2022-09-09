7721124c9755f3a5bb0a93ee237329948c7e00c8
kennedy-ben <makuanikennedy@gmail.com>
2022-09-09 10:58:47 -0700

    last task of advaced

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+* main - Entry
+* Description - Combination of four numbers with space
+* Return: 0 success
+*/
+int main(void)
+{
+int num1 = 0;
+int firstDigit, secondDigit;
+int num2;
+int firstDigit1, secondDigit1;
+while (num1 <= 98)
+{
+firstDigit = (num1 / 10 + 48);
+secondDigit = (num1 % 10 + 48);
+num2 = 0;
+while (num2 <= 99)
+{
+firstDigit1 = (num2 / 10 + 48);
+secondDigit1 = (num2 % 10 + 48);
+if (num1 < num2)
+{
+putchar(firstDigit);
+putchar(secondDigit);
+putchar(' ');
+putchar(firstDigit1);
+putchar(secondDigit1);
+if (num1 != 98)
+{
+putchar(',');
+putchar(' ');
+}}
+num2++;
+}
+num1++;
+}
+putchar('\n');
+return (0);
+}
