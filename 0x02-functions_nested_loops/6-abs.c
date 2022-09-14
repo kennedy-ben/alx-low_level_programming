e15cc66a0db7e1c99899d2c6becc06f0ae4c028e
kennedy-ben <makuanikennedy@gmail.com>
2022-09-14 01:13:22 -0700

    madatory task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* _abs - computes the absolute value of an integer
+* @num : number to check
+* Return:0 or 1
+*/
+int _abs(int num)
+{
+return (num >= 0 ? num : num * -1);
+}
