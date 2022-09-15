e5157216849cb59a96590c491d406c93b9169f02
kennedy-ben <makuanikennedy@gmail.com>
2022-09-15 02:39:30 -0700

    created isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _isdigit -  checks for a digit
+* @c: int type number to check
+* Return: 0 or 1
+*/
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}
