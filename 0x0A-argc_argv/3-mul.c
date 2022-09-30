fa9000ed52dff08b3a5c57dcc44e1218598c8418
kennedy-ben <makuanikennedy@gmail.com>
2022-09-30 10:46:15 -0700

    task 4

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - entry point
+* @argc: number of arguments passed to program
+* @argv: array of strings of arguments passed
+* Return: 0 or 1
+*/
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
